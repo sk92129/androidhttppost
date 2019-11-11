68a90fcd1e3a0006aa23acf2f29a027091732782
seankang-cubic <Lucy1123>
2019-11-11 11:45:44 -0800

    sample post

---
+++ app/src/main/java/com/example/payeezyandroid/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.payeezyandroid;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.os.AsyncTask;
+import org.json.JSONObject;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               doAction();
+            }
+        });
+    }
+
+    private void doAction() {
+        RequestAsync action = new RequestAsync();
+        action.execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private static final String PAYEEZY_URL ="https://api-cert.payeezy.com/v1/transactions";
+
+    public class RequestAsync extends AsyncTask<String,String,String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                //GET Request
+                //return RequestHandler.sendGet("https://prodevsblog.com/android_get.php");
+
+                // POST Request
+                JSONObject postDataParams = new JSONObject();
+                postDataParams.put("name", "Manjeet");
+                postDataParams.put("email", "manjeet@gmail.com");
+                postDataParams.put("phone", "+1111111111");
+
+                return RequestHandler.sendPost(PAYEEZY_URL,postDataParams);
+            }
+            catch(Exception e){
+                return new String("Exception: " + e.getMessage());
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if(s!=null){
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            }
+        }
+
+
+    }
+
+
+}
